22bc6a721ca014a8b6aaf9faab4d23401be3365f
Nimish <knimish322@gmail.com>
2026-09-14 22:02:57 +0530

    Add package.json for server with initial setup and express dependency

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+
+const app = express()
+
+
+
+app.get('/', (req, res) => {
+    res.send(`Hello world !`)
+})
+
+
+app.listen(3000, () => {
+    console.log(`App is listening on https://localhost:3000`)
+})
